fb9640f7447173dab3a41056ae01733041d26ef9
Joel John <johnjoel079@gmail.com>
2024-05-23 10:59:35 -0400

    lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) 
+VALUES('Mr. Chocolate', 5, 'M', 'brown', 'sad', 0), 
+('Rowdy', 9, 'M', 'black', 'happy', 1), 
+('Tabitha', 3, 'F', 'brown', 'angry', 1), 
+('Sergeant Brown', 8, 'M', 'black', 'happy', 0), 
+('Melissa', 7, 'F', 'black', 'sad', 0), 
+('Grinch', 2, 'M', 'polar', 'angry', 1),
+('Wendy', 10, 'F', 'brown', 'happy', 0), 
+(NULL, 6, 'F', 'polar', 'sad', 1);
